98a32d85ad97520b40e8eecb400ca626c01421f0
gradusnik <dvgrad@fxmail.ru>
2021-02-12 18:42:30 +0300

    changed 12.02

--- mod21/task4/main.cpp
+++ mod21/task4/main.cpp
@@ -2,15 +2,16 @@
 
 void evendigits(long long n, int &ans) {
   int cur_digit;
-  while (n > 0) {
+  if (n > 0) {
     cur_digit = n % 10;
     if (cur_digit % 2 == 0) ++ans;
     n /= 10;
+    evendigits(n, ans);
   }
+
 }
 
 int main() {
-  long long n;
   int ans(0);
   evendigits(9223372036854775806, ans);
   std::cout << "num of even digits from N is " << ans;
