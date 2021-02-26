313bddef50240c548c096153dcf012a1146cab9b
gradusnik <dvgrad@fxmail.ru>
2021-02-26 22:58:58 +0300

    t2 added

---
+++ mod23/task2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+  std::srand(std::time(nullptr));
+  std::ofstream pic("picture.txt");
+  if (pic.is_open()) {
+    int r_num;
+    int width(0), height(0);
+    std::cout << "enter the width of picture: ";
+    std::cin >> width;
+    std::cout << "enter the height of picture: ";
+    std::cin >> height;
+    for (int i = 0; i < height; ++i) {
+      for (int j = 0; j < width; ++j) {
+        pic << (r_num = std::rand() % 2);
+      }
+      pic << '\n';
+    }
+  }
+  return 0;
+}
