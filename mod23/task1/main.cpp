a47045c8a6266951767d00a1e0866e508c834f58
gradusnik <dvgrad@fxmail.ru>
2021-02-26 22:17:33 +0300

    m23 t1

---
+++ mod23/task1/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+std::ofstream in_file("table01.txt", std::ios::app);
+
+void writing (std::string message, char c) {
+  std::string line;
+  std::cout << message;
+  std::cin >> line;
+  in_file << line << c;
+}
+
+int main() {
+  std::string line;
+  int answ(1);
+  if (in_file.is_open()) {
+    while (answ) {
+      std::cout << "would you like to make one more note?\n";
+      std::cout << "if YES - press 1(and enter), if NO - press 0(and enter): ";
+      std::cin >> answ;
+      if (answ) {
+        writing("enter the first name: ", ' ');
+        writing("enter the last name: ", ' ');
+        writing("enter the date(format dd.mm.yyyy): ", ' ');
+        writing ("enter the sum: ", '\n');
+      }
+    }
+    in_file.close();
+  }
+  return 0;
+}
