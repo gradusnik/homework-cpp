373e15c0464af17ca2643bfb67a5a950b84e66b2
gradusnik <dvgrad@fxmail.ru>
2021-03-29 21:18:57 +0300

    t4 changed 29.03

--- mod24/task4/main.cpp
+++ mod24/task4/main.cpp
@@ -9,92 +9,83 @@ struct character {
     int health = 50;
     int armor = 0;
     int damage = 15;
-};
-
-struct coordinate {
     int x = 0;
     int y = 0;
 };
 
-void checkAvailableCoordinates(int &x, int &y, coordinate coords[]) {
-  for (int i(0); i < 6; ++i) {
-    if (x == coords[i].x && y == coords[i].y) {
+void checkAvailableCoordinates(int &x, int &y, character chars[], int num) {
+  for (int i(0); i < num; ++i) {
+    if (x == chars[i].x && y == chars[i].y) {
       ++x; ++y;
-      checkAvailableCoordinates(x, y, coords);
+      checkAvailableCoordinates(x, y, chars, num);
     }
   }
 }
 
-bool coincide(int n, int m , coordinate coords[], character chars[]) {
-  for (int i(0); i < 5; ++i) {
-    if (coords[i].x == m && coords[i].y == n && chars[i].health > 0) return true;
+bool coincide(int n, int m, character chars[], int player) {
+  for (int i(0); i < player; ++i) {
+    if (chars[i].x == m && chars[i].y == n && chars[i].health > 0) return true;
   }
   return false;
 }
 
-void displayField(coordinate coords[], character chars[]) {
+void displayField(character chars[], int player) {
   for (int i(0); i < 40; ++i) {
     for (int j(0); j < 40; ++j) {
-      if (coincide(i, j, coords, chars)) std::cout << 'E';
-      else if (i == coords[5].y && j == coords[5].x) std::cout << 'P';
+      if (coincide(i, j, chars, player)) std::cout << 'E';
+      else if (i == chars[player].y && j == chars[player].x) std::cout << 'P';
       else std::cout << '.';
     }
     std::cout << std::endl;
   }
 }
 
-void saving(std::ofstream &file, character arr[], coordinate coords[]) {
-  for (int i = 0; i < 6; ++i) {
-    file.write((char*)&arr[i], sizeof arr[i]);
-  }
-  for (int i = 0; i < 6; ++i) {
-    file.write((char*)&coords[i], sizeof coords[i]);
+void saving(std::ofstream &file, character chars[], int amount) {
+  for (int i = 0; i < amount; ++i) {
+    file.write((char*)&chars[i], sizeof chars[i]);
   }
 }
 
-void loading(std::ifstream &file, character arr[], coordinate coords[]) {
-  for (int i = 0; i < 6; ++i) {
-    file.read((char*)&arr[i], sizeof arr[i]);
-  }
-  for (int i = 0; i < 6; ++i) {
-    file.read((char*)&coords[i], sizeof coords[i]);
+void loading(std::ifstream &file, character chars[], int amount) {
+  for (int i = 0; i < amount; ++i) {
+    file.read((char*)&chars[i], sizeof chars[i]);
   }
 }
 
-bool emptyDir(const std::string &direction, int ch, coordinate coords[], character chars[]) {
+bool emptyDir(const std::string &direction, int ch, character chars[], int player) {
   if (direction == "left") {
-    if (coords[ch].x == 0) return false;
-    for (int i = 0; i < 5; ++i) {
+    if (chars[ch].x == 0) return false;
+    for (int i = 0; i < player; ++i) {
       if (i == ch) continue;
-      else if (coords[i].x == coords[ch].x - 1 && coords[i].y == coords[ch].y && chars[i].health > 0) return false;
+      else if (chars[i].x == chars[ch].x - 1 && chars[i].y == chars[ch].y && chars[i].health > 0) return false;
     }
     return true;
   } else if (direction == "top") {
-    if (coords[ch].y == 0) return false;
-    for (int i = 0; i < 5; ++i) {
+    if (chars[ch].y == 0) return false;
+    for (int i = 0; i < player; ++i) {
       if (i == ch) continue;
-      else if (coords[i].x == coords[ch].x && coords[i].y == coords[ch].y - 1 && chars[i].health > 0) return false;
+      else if (chars[i].x == chars[ch].x && chars[i].y == chars[ch].y - 1 && chars[i].health > 0) return false;
     }
     return true;
   } else if (direction == "right") {
-    if (coords[ch].x == 39) return false;
-    for (int i = 0; i < 5; ++i) {
+    if (chars[ch].x == 39) return false;
+    for (int i = 0; i < player; ++i) {
       if (i == ch) continue;
-      else if (coords[i].x == coords[ch].x + 1 && coords[i].y == coords[ch].y && chars[i].health > 0) return false;
+      else if (chars[i].x == chars[ch].x + 1 && chars[i].y == chars[ch].y && chars[i].health > 0) return false;
     }
     return true;
   } else if (direction == "bottom") {
-    if (coords[ch].y == 39) return false;
-    for (int i = 0; i < 5; ++i) {
+    if (chars[ch].y == 39) return false;
+    for (int i = 0; i < player; ++i) {
       if (i == ch) continue;
-      else if (coords[i].x == coords[ch].x && coords[i].y == coords[ch].y + 1 && chars[i].health > 0) return false;
+      else if (chars[i].x == chars[ch].x && chars[i].y == chars[ch].y + 1 && chars[i].health > 0) return false;
     }
     return true;
   }
   return false;
 }
 
-void damage(character chars[],const int &damagedChar, const int &hittingChar = 5){
+void damage(character chars[],const int &damagedChar, const int &hittingChar){
   if (chars[damagedChar].armor > chars[hittingChar].damage) {
     chars[damagedChar].armor -= chars[hittingChar].damage;
   } else {
@@ -103,55 +94,55 @@ void damage(character chars[],const int &damagedChar, const int &hittingChar = 5
   }
 }
 
-void makeTurn(int charNum, coordinate coords[], character chars[], const std::string &direction) {
+void makeTurn(int charNum, character chars[], const std::string &direction, int player) {
   bool turned = false;
-  if (charNum == 5) {
+  if (charNum == player) {
     if (direction == "left") {
-      for (int i = 0; i < 5; ++i) {
-        if (coords[i].x == coords[5].x - 1 && coords[i].y == coords[5].y && chars[i].health > 0) {
-          damage(chars, i);
+      for (int i = 0; i < player; ++i) {
+        if (chars[i].x == chars[player].x - 1 && chars[i].y == chars[player].y && chars[i].health > 0) {
+          damage(chars, i, player);
           turned = true;
         }
       }
-      if (!turned && coords[5].x > 0) --coords[5].x;
+      if (!turned && chars[player].x > 0) --chars[player].x;
     } else if (direction == "top") {
-      for (int i = 0; i < 5; ++i) {
-        if (coords[i].x == coords[5].x && coords[i].y == coords[5].y - 1 && chars[i].health > 0) {
-          damage(chars, i);
+      for (int i = 0; i < player; ++i) {
+        if (chars[i].x == chars[player].x && chars[i].y == chars[player].y - 1 && chars[i].health > 0) {
+          damage(chars, i, player);
           turned = true;
         }
       }
-      if (!turned && coords[5].y > 0) --coords[5].y;
+      if (!turned && chars[player].y > 0) --chars[player].y;
     } else if (direction == "right") {
-      for (int i = 0; i < 5; ++i) {
-        if (coords[i].x == coords[5].x + 1 && coords[i].y == coords[5].y && chars[i].health > 0) {
-          damage(chars, i);
+      for (int i = 0; i < player; ++i) {
+        if (chars[i].x == chars[player].x + 1 && chars[i].y == chars[player].y && chars[i].health > 0) {
+          damage(chars, i, player);
           turned = true;
         }
       }
-      if (!turned && coords[5].x < 39) ++coords[5].x;
+      if (!turned && chars[player].x < 39) ++chars[player].x;
     } else if (direction == "bottom") {
-      for (int i = 0; i < 5; ++i) {
-        if (coords[i].x == coords[5].x && coords[i].y == ++coords[5].y && chars[i].health > 0) {
-          damage(chars, i);
+      for (int i = 0; i < player; ++i) {
+        if (chars[i].x == chars[player].x && chars[i].y == chars[player].y + 1 && chars[i].health > 0) {
+          damage(chars, i, player);
           turned = true;
         }
       }
-      if (!turned && coords[5].y < 39) ++coords[5].y;
+      if (!turned && chars[player].y < 39) ++chars[player].y;
     }
   } else {
     if (direction == "left") {
-      if (coords[5].x == coords[charNum].x - 1 && coords[5].y == coords[charNum].y && chars[5].health > 0) damage(chars, 5, charNum);
-      else if (emptyDir(direction, charNum, coords, chars)) --coords[charNum].x;
+      if (chars[player].x == chars[charNum].x - 1 && chars[player].y == chars[charNum].y && chars[player].health > 0) damage(chars, player, charNum);
+      else if (emptyDir(direction, charNum, chars, player)) --chars[charNum].x;
     } else if (direction == "top") {
-      if (coords[5].x == coords[charNum].x && coords[5].y == coords[charNum].y - 1 && chars[5].health > 0) damage(chars, 5, charNum);
-      else if (emptyDir(direction, charNum, coords, chars)) --coords[charNum].y;
+      if (chars[player].x == chars[charNum].x && chars[player].y == chars[charNum].y - 1 && chars[player].health > 0) damage(chars, player, charNum);
+      else if (emptyDir(direction, charNum, chars, player)) --chars[charNum].y;
     } if (direction == "right") {
-      if (coords[5].x == coords[charNum].x + 1 && coords[5].y == coords[charNum].y && chars[5].health > 0) damage(chars, 5, charNum);
-      else if (emptyDir(direction, charNum, coords, chars)) ++coords[charNum].x;
+      if (chars[player].x == chars[charNum].x + 1 && chars[player].y == chars[charNum].y && chars[player].health > 0) damage(chars, player, charNum);
+      else if (emptyDir(direction, charNum, chars, player)) ++chars[charNum].x;
     } if (direction == "bottom") {
-      if (coords[5].x == coords[charNum].x && coords[5].y == coords[charNum].y + 1 && chars[5].health > 0) damage(chars, 5, charNum);
-      else if (emptyDir(direction, charNum, coords, chars)) ++coords[charNum].y;
+      if (chars[player].x == chars[charNum].x && chars[player].y == chars[charNum].y + 1 && chars[player].health > 0) damage(chars, player, charNum);
+      else if (emptyDir(direction, charNum, chars, player)) ++chars[charNum].y;
     }
   }
 }
@@ -161,12 +152,12 @@ std::string genDir() {
   if (n == 0) return "left";
   else if (n == 1) return "top";
   else if (n == 2) return "right";
-  else if (n == 3) return "bottom";
+  else return "bottom";
 }
 
-bool checkWin(character chars[]) {
-  if (chars[5].health <= 0) return true;
-  for (int i = 0; i < 5; ++i) {
+bool checkWin(character chars[], int player) {
+  if (chars[player].health <= 0) return true;
+  for (int i = 0; i < player; ++i) {
     if (chars[i].health > 0) return false;
   }
   return true;
@@ -174,69 +165,70 @@ bool checkWin(character chars[]) {
 
 int main() {
   std::srand(std::time(nullptr));
-  coordinate fieldState[6];
-  character characters[6];
+  int charsAmount = 6;
+  character characters[charsAmount];
+  int player = 5;
   // enemies data
-  for (int i(0); i < 5; ++i) {
+  for (int i(0); i < player; ++i) {
     characters[i].name = "Enemy #" + std::to_string(i);
-    characters[i].health = 50 + std::rand() % 100;
-    characters[i].armor = std::rand() % 50;
-    characters[i].damage = 15 + std::rand() % 15;
+    characters[i].health = 50 + std::rand() % 101;
+    characters[i].armor = std::rand() % 51;
+    characters[i].damage = 15 + std::rand() % 16;
   }
   // player data (player - characters[5])
   std::cout << "\tcreating the player character ..." << std::endl;
   std::cout << "enter the player name: ";
-  std::cin >> characters[5].name;
+  std::cin >> characters[player].name;
   std::cout << "enter the player health: ";
-  std::cin >> characters[5].health;
+  std::cin >> characters[player].health;
   std::cout << "enter the player armor: ";
-  std::cin >> characters[5].armor;
+  std::cin >> characters[player].armor;
   std::cout << "enter the player damage: ";
-  std::cin >> characters[5].damage;
+  std::cin >> characters[player].damage;
   std::cout << "enter the player's coordinates(0 - 39): " << std::endl;
   std::cout << "\tx: ";
-  std::cin >> fieldState[5].x; fieldState[5].x %= 40;
+  std::cin >> characters[player].x; characters[player].x %= 40;
   std::cout << "\ty: ";
-  std::cin >> fieldState[5].y; fieldState[5].y %= 40;
+  std::cin >> characters[player].y; characters[player].y %= 40;
   // coordinates for enemies
-  for (int i(0); i < 5; ++i) {
+  for (int i(0); i < player; ++i) {
     int x = std::rand() % 40;
     int y = std::rand() % 40;
-    checkAvailableCoordinates(x, y, fieldState);
-    fieldState[i].x = x;
-    fieldState[i].y = y;
+    checkAvailableCoordinates(x, y, characters, charsAmount);
+    characters[i].x = x;
+    characters[i].y = y;
   }
-  displayField(fieldState, characters);
+  displayField(characters, player);
   bool win = false;
   std::string command;
   while (!win) {
-    std::cout << characters[5].name << " choose the command: ";
+    std::cout << characters[player].name << " choose the command: ";
     std::cin >> command;
     if (command == "save") {
       std::ofstream file1("save.bin", std::ios::binary);
       if (file1.is_open()) {
-        saving(file1, characters, fieldState);
+        saving(file1, characters, charsAmount);
         file1.close();
-      }
+      } else std::cerr << "file not found" << std::endl;
     } else if (command == "load") {
       std::ifstream file2("save.bin", std::ios::binary);
       if (file2.is_open()) {
-        loading(file2, characters, fieldState);
+        loading(file2, characters, charsAmount);
         file2.close();
-      }
-      displayField(fieldState, characters);
+      } else std::cerr << "file not found" << std::endl;
+      displayField(characters, player);
     } else if (command == "left" || command == "top" || command == "right" || command == "bottom") {
-      makeTurn(5, fieldState, characters, command);
-      for (int i = 0; i < 5; ++i) {
+      makeTurn(player, characters, command, player);
+      for (int i = 0; i < player; ++i) {
         if (characters[i].health > 0) {
           std::string direction = genDir();
-          makeTurn(i, fieldState, characters, direction);
+          makeTurn(i, characters, direction, player);
         }
       }
-      displayField(fieldState, characters);
-      win = checkWin(characters);
+      displayField(characters, player);
+      win = checkWin(characters, player);
     } else std::cerr << "there is not such command!" << std::endl;
   }
-  (characters[5].health > 0) ? std::cout << "player win" : std::cout << "player lose";
+  std::cout << ((characters[player].health > 0) ?  "player win" :  "player lose");
   return 0;
 }
