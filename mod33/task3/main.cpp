8073822d8828a4a8ad0d0aee603e6f6972ba7fff
gradusnik <dvgrad@fxmail.ru>
2022-03-22 20:20:05 +0300

    template01

--- mod33/task3/main.cpp
+++ mod33/task3/main.cpp
@@ -22,57 +22,59 @@ public:
       val = in.val;
     }
 };
-
 template<typename K, typename V>
-void addElem(std::vector<Pair<K, V>> &vec) {
-  std::cout << std::endl << "adding element" << std::endl;
-  K in_key;
-  V in_val;
-  std::cout << "enter the element key: ";
-  std::cin >> in_key;
-  std::cout << "enter the element value: ";
-  std::cin >> in_val;
-  Pair tmp(in_key, in_val);
-  vec.push_back(tmp);
-}
-template<typename K, typename V>
-void removeElement(std::vector<Pair<K, V>> &vec) {
-  std::cout << std::endl << "removing elements" << std::endl;
-  K in_key;
-  std::cout << "enter the element key you wanna erase: ";
-  std::cin >> in_key;
-  for (auto it = vec.begin(); it != vec.end(); ++it) {
-    if (it->getKey() == in_key) {
-      vec.erase(it); --it;
+class BUF {
+    std::vector<Pair<K, V>> b;
+public:
+    void addElem() {
+      std::cout << std::endl << "adding element" << std::endl;
+      K in_key;
+      V in_val;
+      std::cout << "enter the element key: ";
+      std::cin >> in_key;
+      std::cout << "enter the element value: ";
+      std::cin >> in_val;
+      Pair tmp(in_key, in_val);
+      b.push_back(tmp);
     }
-  }
-}
-template<typename K, typename V>
-void printElements(std::vector<Pair<K, V>> &vec) {
-  std::cout << std::endl;
-  std::cout << "\'key\'" << "\t" << "\'value\'" << std::endl;
-  for (auto item = vec.begin(); item != vec.end(); ++item) {
-    std::cout << item->getKey() << "\t" << item->getVal() << std::endl;
-  }
-}
-template<typename K, typename V>
-void findElement(std::vector<Pair<K, V>> &vec) {
-  std::vector<Pair<K, V>> rez;
-  K in_key;
-  std::cout << std::endl << "enter the element key you wanna find: ";
-  std::cin >> in_key;
-  for (auto item = vec.begin(); item != vec.end(); ++item) if (item->getKey() == in_key) rez.push_back(*item);
-  if (rez.empty()) std::cout << "no elements" << std::endl;
-  else {
-    std::cout << "there are following elements: " << std::endl;
-    printElements(rez);
-  }
-}
+    void removeElement() {
+      std::cout << std::endl << "removing elements" << std::endl;
+      K in_key;
+      std::cout << "enter the element key you wanna erase: ";
+      std::cin >> in_key;
+      for (auto it = b.begin(); it != b.end(); ++it) {
+        if (it->getKey() == in_key) {
+          b.erase(it); --it;
+        }
+      }
+    }
+    void printElements() {
+      std::cout << std::endl;
+      std::cout << "\'key\'" << "\t" << "\'value\'" << std::endl;
+      for (auto item = b.begin(); item != b.end(); ++item) {
+        std::cout << item->getKey() << "\t" << item->getVal() << std::endl;
+      }
+    }
+    void findElement() {
+      std::vector<Pair<K, V>> rez;
+      K in_key;
+      std::cout << std::endl << "enter the element key you wanna find: ";
+      std::cin >> in_key;
+      for (auto item = b.begin(); item != b.end(); ++item) if (item->getKey() == in_key) rez.push_back(*item);
+      if (rez.empty()) std::cout << "no elements" << std::endl;
+      else {
+        std::cout << "there are following elements: " << std::endl;
+        std::cout << "\'key\'" << "\t" << "\'value\'" << std::endl;
+        for (auto item = rez.begin(); item != rez.end(); ++item) {
+          std::cout << item->getKey() << "\t" << item->getVal() << std::endl;
+        }
+      }
+    }
+};
 
 int main() {
 
-  std::vector<Pair<parK, parV>> buf;
-
+  BUF<parK, parV> buf;
   int select(-1);
   while (select) {
     std::cout << std::endl << "you wanna: " << std::endl;
@@ -83,10 +85,10 @@ int main() {
     std::cout << "0 for exit" << std::endl;
     std::cout << "...: ";
     std::cin >> select;
-    if (select == 1) addElem(buf);
-    else if (select == 2) removeElement(buf);
-    else if (select == 3) printElements(buf);
-    else if (select == 4) findElement(buf);
+    if (select == 1) buf.addElem();
+    else if (select == 2) buf.removeElement();
+    else if (select == 3) buf.printElements();
+    else if (select == 4) buf.findElement();
     else if (select == 0) std::cout << "exit" << std::endl;
     else std::cout << "no option" << std::endl;
   }
